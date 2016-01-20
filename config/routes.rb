de95678bff7423f0c0c59e1ab2e8d64fccec91ef
Priyanka Saigal <email": null>
2016-01-20 15:15:25 -0800

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'blog/index'
+  # get 'blog/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
